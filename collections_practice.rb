074f6889d8992eb6e38113eeaab0e4f1bcdbe154
Colin McKean <ezikill@hotmail.com>
2017-09-08 19:14:30 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,19 @@
 def sort_array_asc(array)
   array.sort
 end
+
+def sort_array_desc(array)
+  array
+end
+
+def sort_array_char_count(array)
+  array
+end
+
+def kesha_maker()
+  
+end
+
+def find_a(array)
+  
+end
